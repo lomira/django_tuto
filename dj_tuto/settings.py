46e1d605af23ba7c5bfef5e42596949d7281df99
lomi <lomi@lomag.com>
2020-10-08 16:19:54 +0200

    hide secret key

--- dj_tuto/settings.py
+++ dj_tuto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dj_tuto.secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yk#5=5u1p^a)p(40g)g@co+8gx4uw6&6mmqax6s9x5a_m(f%g='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
